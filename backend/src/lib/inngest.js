08f0a69a4ace5d1ae2dc8343d30ef0ac0edaa869
Mr-Shamuel <shamuelsam300@gmail.com>
2025-12-17 01:32:46 +0600

    sync auth with db

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,41 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../model/User.js";
+
+export const inngest = new Inngest({ id: "talent-id" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_addresses,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: image_url,
+    };
+
+    await User.create(newUser);
+
+    //todo : do somethings 
+  }
+);
+
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.delted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+
+    //todo : do somethings
+  }
+);
+
+export const functions = [syncUser,deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,39 +1,43 @@
-import experess from "express";
+import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./lib/inngest.js";
 
-const app = experess();
+const app = express();
 
 const __dirname = path.resolve();
+//middleware
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success api is up and running ", status: 200 });
 });
-app.get("/books",(req,res)=>{
-  res.status(200).json({msg:"this is books endpoint"})
-})
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is books endpoint" });
+});
 
-// make our app ready for developement  
-if(ENV.NODE_ENV==='production'){
-  app.use(experess.static(path.join(__dirname,"../frontend/dist")))
-  app.get("/{*any}",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist",""));
-  })
+// make our app ready for developement
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", ""));
+  });
 }
 
-const startServer=async ()=>{
+const startServer = async () => {
   try {
     await connectDB();
-  app.listen(ENV.PORT, () => {
-    console.log("listening to the port", ENV.PORT);
-  });
+    app.listen(ENV.PORT, () => {
+      console.log("listening to the port", ENV.PORT);
+    });
   } catch (error) {
-    console.log('Error Starting  the server',error)
+    console.log("Error Starting  the server", error);
   }
-
-}
+};
 
 startServer();
-
-
